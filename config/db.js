b1e771d2aa515718ebd94c5c5cafdf6ba5b2ddda
Ramdhan  Rizki <ramdhanrizki11@gmail.com>
2016-08-18 00:04:53 +0000

    db.js Edit Koneksi Database

--- config/db.js
+++ config/db.js
@@ -10,10 +10,10 @@ var mysql = require("mysql");
 
  var konek = {
    host     : 'ap-cdbr-azure-southeast-b.cloudapp.net',
-   user     : 'b8bf14d452d4b5',
-   password : 'b1476b57',
+   user     : 'b065bc94f582d8',
+   password : '67928ce1',
    port 	: 3306,
-   database : 'comradedb'
+   database : 'acsm_960a6532c696724'
  };
 
 /*var konek = {
