29d265e4d52a8cd09f05e47db131ed70af31209e
Tioreza <tiorezafk@yahoo.com>
2016-08-24 20:43:27 +0700

    fix auth google

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '682030206564-5g9tdtfck0lqdblku8vqquufa0hb4tq9.apps.googleusercontent.com',
         'clientSecret'  : 'jnZPk_Rk3gXZ0gZMbIw_i8hf',
-        'callbackURL'   : 'comrade-api.azurewebsites.net/auth/google/callback'
+        'callbackURL'   : 'http://comrade-api.azurewebsites.net/auth/google/callback'
     }
 
 };
