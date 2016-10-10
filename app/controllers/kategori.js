7f291cf620a182e89401ed2c46d4a76a36b1a8fa
Tioreza Febrian <Tioreza Febrian>
2016-10-09 19:08:11 -0700

    edit kategori

--- app/controllers/kategori.js
+++ app/controllers/kategori.js
@@ -9,12 +9,15 @@ this.kategori = function(req,res,next) {
 			con.release();
 			con.query('SELECT * FROM posting WHERE id_kategori=2',function(err,artikel){
 				con.query('SELECT * FROM posting WHERE id_kategori=1',function(err,berita){
-					kategori[0].jml_post=berita.length;
-					kategori[1].jml_post=artikel.length;
-					if(err)
-						res.json({status:'400',message:err.code,result:[]});
-					else
-						res.json({status:'200',message:'success',result:kategori});
+					con.query('SELECT * FROM event',function(err,event){
+						kategori[0].jml_post=berita.length;
+						kategori[1].jml_post=artikel.length;
+						kategori[2].jml_post=event.length;
+						if(err)
+							res.json({status:'400',message:err.code,result:[]});
+
+							res.json({status:'200',message:'success',result:kategori});
+					});
 				});
 			});
 		});
