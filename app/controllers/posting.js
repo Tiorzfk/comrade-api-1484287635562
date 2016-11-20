ac10370c61b7e47d50ebb15af27ec0d44e5ccb89
tiorzfk <tiorezafk@yahoo.com>
2016-11-19 21:27:07 -0800

    rss sciencedialy

--- app/controllers/posting.js
+++ app/controllers/posting.js
@@ -1,4 +1,5 @@
 var db = require('../../config/db');
+var moment = require('moment');
 var Sync = require('sync');
 var xml2js = require('xml2js');
 var parser = new xml2js.Parser();
@@ -7,7 +8,7 @@ var striptags = require('striptags');
 
 function Todo() {
 
-function cekLiputan(req,res2,next) {
+function rssLiputan(req,res2,next) {
   parser.on('error', function(err) { console.log('Parser error', err); });
 
  var data = '';
@@ -29,7 +30,7 @@ function cekLiputan(req,res2,next) {
                status : '0',
                id_kategori : 1,
                id_admin : 1,
-               tgl_posting : result.rss.channel[0].item[1].pubDate[0]
+               tgl_posting : result.rss.channel[0].item[0].pubDate[0]
              }
              //con.query('SELECT * FROM posting WHERE id_posting='+data.id,function(err,data){
                //if(!data.length){
@@ -50,7 +51,54 @@ function cekLiputan(req,res2,next) {
    });
 }
 
-//setInterval(cekLiputan, /*60000 * 60*/10000);
+function rssSciencedaily(req, res2, next) {
+  parser.on('error', function(err) { console.log('Parser error', err); });
+
+ var data = '';
+ http.get('http://rss.sciencedaily.com/health_medicine/hiv_and_aids.xml', function(res) {
+     if (res.statusCode >= 200 && res.statusCode < 400) {
+       res.on('data', function(data_) { data += data_.toString(); });
+       res.on('end', function() {
+         parser.parseString(data, function(err, result) {
+           db.acquire(function(err,con){
+             if (err) throw err;
+             var arrayisi = striptags(result.rss.channel[0].item[0].description[0]).split(' ');
+             var sliceisi = arrayisi.slice(0,17);
+             var data = {
+               //id : result.rss.channel[0].item[0].guid[0]._,
+               judul : result.rss.channel[0].item[0].title[0],
+               deskripsi : sliceisi.join(' '),
+               isi : result.rss.channel[0].item[0].description[0]+'<a href='+result.rss.channel[0].item[0].guid[0]._+'> more </a>',
+               foto : result.rss.channel[0].item[0]['media:thumbnail'][0].$.url,
+               status : '0',
+               id_kategori : 2,
+               id_admin : 1,
+               tgl_posting : moment().format('YYYY-MM-DD'),
+               sumber : result.rss.channel[0].item[0].guid[0]._
+             }
+             //return res2.json(moment().format('YYYY-MM-DD'));
+
+             con.query('SELECT * FROM posting WHERE judul="'+data.judul+'"',function(err,data){
+               if(!data.length){
+                 con.query('INSERT INTO posting SET ?',data,function(err,data){
+                   con.release();
+                   if (err)
+                      console.log(err);
+
+                   console.log('Berhasil menambah data');
+                 })
+               }
+             });
+           });
+           //return res2.json([data]);
+         });
+       });
+     }
+   });
+}
+
+//setInterval(rssLiputan, /*60000 * 60*/10000);
+setInterval(rssSciencedaily, /*60000 * 60*/10000);
 
 this.posting = function(req, res, next) {
     var jml = 0;
