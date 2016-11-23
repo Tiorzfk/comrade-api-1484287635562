96e09e514b2f5d7abd32119ec810b363d15035c5
tiorzfk <tiorezafk@yahoo.com>
2016-11-23 14:49:58 -0800

    edit auth eroorrr

--- app/controllers/sahabatodha.js
+++ app/controllers/sahabatodha.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 var moment = require('moment');
 var AES = require('./AES');
 var bcrypt = require('bcrypt-nodejs');
+var jwt = require('jsonwebtoken');
+var EmailTemplates = require('swig-email-templates');
+var transport = require('../../config/mail').transport;
 
 function randomkey()
 {
@@ -257,10 +260,46 @@ this.daftarsa = function(req, res, next) {
           id_user : result.insertId
         }
         con.query('INSERT INTO sahabat_odha SET ? ',data2,function(err,result2){
-          return res.json({
-            result: 'Created',
-            status: 200,
-            message: 'Registration is successful.'
+          var token = jwt.sign(data, 'emailconfirmationcomrade', {
+            expiresIn: "2h" // expires in 24 hours
+          });
+          var templates = new EmailTemplates({root: 'app/views/emails'});
+          var locals = {
+              email: req.body.email,
+              token: token,
+              url: 'http://comrade-api.azurewebsites.net/confirm'
+          };
+
+          templates.render('confirm-email.html', locals, function(err, html) {
+              if (err) {
+                return res.json({
+                  result: 'Failed',
+                  status: 403,
+                  errors: err,
+                });
+              } else {
+                  transport.sendMail({
+                      from: 'Comrade app <Admin@comrade.com>',
+                      to: locals.email,
+                      subject: 'Confirmation Email.',
+                      html: html
+                      }, function(err, responseStatus) {
+                          if (err) {
+                              return res.json({
+                                result: 'Failed',
+                                status: 403,
+                                errors: err,
+                              });
+                          } else {
+                              return res.status(201).send({
+                                result: 'Created',
+                                status: 201,
+                                message: 'Registration is successful, check your email to activate your account.'
+                              });
+                          }
+                      }
+                  );
+              }
           });
         });
       });
