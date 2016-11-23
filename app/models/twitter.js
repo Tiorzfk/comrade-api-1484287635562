62e665902fd7c35eda8df416ded77cd24d815f38
Ramdhan Rizki <Ramdhan Rizki>
2016-11-23 22:41:05 +0700

    Ubah tampil tweet eng

---
+++ app/models/twitter.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema =  mongoose.Schema;
+TweettrainSchema = new Schema({
+  id_tweet:String,
+  id_string :String,
+  screen_name:String,
+  text:String,
+  profile_image_url:String,
+  created_at:Date, 
+  klasifikasi:String,
+  profile_link_color:String,
+  status:String,
+  words:[String],
+  statusToken:Number,
+});
+
+module.exports = mongoose.model("Tweetmining",TweettrainSchema);
