c06c5f38c3e8773aa03e3b6d4f5770ec4eb23e8b
tiorzfk <tiorezafk@yahoo.com>
2016-12-20 18:17:08 -0800

    edit error

--- app/routes/healbox.js
+++ app/routes/healbox.js
@@ -5,7 +5,7 @@ module.exports = {
     app.route('/reminder').get(healbox.reminder).post(healbox.reminderPost);
 
     app.route('/healbox').post(healbox.updateID);
-d
+
     app.route('/arv_reminder').post(healbox.arv_reminder);
 
   }
