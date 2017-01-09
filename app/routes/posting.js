b0c02eaa297fbd91f5ed47a826edf7f046c2f0de
tiorzfk <tiorezafk@yahoo.com>
2017-01-09 14:13:32 -0800

    edit comment posting

--- app/routes/posting.js
+++ app/routes/posting.js
@@ -4,9 +4,9 @@ module.exports = {
   configure: function(app) {
     app.route('/posting/page/:page').get(posting.posting);
 
-    app.route('/posting/kategori/:kategori').get(posting.kategoriAll);
-    app.route('/posting/kategori/:kategori/page/:page').get(posting.kategori);
-    app.route('/posting/kategori/:kategori/:lang/page/:page').get(posting.postLang);
+    // app.route('/posting/kategori/:kategori').get(posting.kategoriAll);
+    // app.route('/posting/kategori/:kategori/page/:page').get(posting.kategori);
+    // app.route('/posting/kategori/:kategori/:lang/page/:page').get(posting.postLang);
 
     //app.route('/posting/:id').get(posting.postingID).post(posting.editPosting);
 
